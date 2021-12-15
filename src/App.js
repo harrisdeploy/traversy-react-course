5d669fd9c81cf166c89039ba3b353fd5e0c3059f
harris <haarris82@gmail.com>
2021-12-15 18:17:19 +1100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDom from 'react-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='container'>
+            <h1> My App </h1>
+        </div>
+    )
 }
 
 export default App;
+
+// function App() {
+//     const title = 'My Title';
+//     const body = 'My Body';
+
+//     const commentsList = [
+//         {id: '1', text: 'hello comment 1' },
+//         {id: '2', text: 'hello comment 2' },
+//         {id: '3', text: 'hello comment 3' },
+//     ]
+
+//     const loading = false, showComments = true;
+    
+//     if (loading) {
+//         return (<h1>Loading...</h1>)
+//     }
+
+//     const commentBlock = (
+//             <div className='comments'>
+//                 <h3>Comments length: ({commentsList.length})</h3>
+//                 <ul>
+//                     {commentsList.map((comment, index) => (
+//                         <li key={index}>{comment.text}</li>
+//                     ))}
+//                 </ul>
+//             </div> 
+//     )
+
+//     return (
+//         <div className='container'>
+//             <h1> {title.toUpperCase()} </h1>
+//             <p>{body}</p>
+
+//             {/*showComments ? (
+//                 'showComments is set to true'
+//                 ) 
+//             : (
+//                 'showComments is set to false'
+//             )*/}
+//             {/*showComments && (
+//                 'showComments is set to true automatically lest set to false'
+//                 ) 
+//             */} 
+    
+//             {/*showComments && commentBlock
+//             */} 
+
+            
+//             {showComments && commentBlock }
+//         </div>
+//     )
+// }
